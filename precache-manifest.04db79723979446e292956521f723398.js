bb4f2de79fd951d86a96a3985cb77e1ad660dbc1
Jaf <jaf@epns.io>
2022-04-06 18:07:12 -0600

    Updates

---
+++ precache-manifest.04db79723979446e292956521f723398.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d67cc78a82d9d723c742f8c9366a1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce45339163fcfb91228f",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "8c51a4e8ec2af1ffbff9",
+    "url": "/static/css/main.b95ae1e4.chunk.css"
+  },
+  {
+    "revision": "ce45339163fcfb91228f",
+    "url": "/static/js/2.16cada80.chunk.js"
+  },
+  {
+    "revision": "ccc2543c268a0e6b5e88204c6551a8d9",
+    "url": "/static/js/2.16cada80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c51a4e8ec2af1ffbff9",
+    "url": "/static/js/main.c19237c5.chunk.js"
+  },
+  {
+    "revision": "3e234450efee118b7e12",
+    "url": "/static/js/runtime-main.fb220953.js"
+  },
+  {
+    "revision": "7b72c949c0bd79455f0cf17e09ec4269",
+    "url": "/static/media/gradient.7b72c949.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b32830d2636e7a67511f9c763728df83.js"
+  "/precache-manifest.04db79723979446e292956521f723398.js"
 );
 
 self.addEventListener('message', (event) => {
