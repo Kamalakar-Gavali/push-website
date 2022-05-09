cd511241b9ba9220974eea0e0ad47e507c3f8574
Abishek Bashyal <abishek_bashyal@outlook.com>
2022-05-09 17:39:06 +0545

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d057bd9fa31926a5ebb371cc021b72bd.js"
+  "/precache-manifest.7a143a6af8fe6031718e84c4ad36a38b.js"
 );
 
 self.addEventListener('message', (event) => {
