f43f2d0a048338fa6151bb7d2e0a081470c71fa9
Jaf <jaf@epns.io>
2022-05-14 19:15:11 -0600

    Updates

---
+++ precache-manifest.1bc959538705da256c26734c14cbb6cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a143962be6d2272f8ba0c42de7cf07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d0943d1714b461e048e",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "00bd36ec5670e33993d1",
+    "url": "/static/css/main.b95ae1e4.chunk.css"
+  },
+  {
+    "revision": "1d0943d1714b461e048e",
+    "url": "/static/js/2.6885500a.chunk.js"
+  },
+  {
+    "revision": "ccc2543c268a0e6b5e88204c6551a8d9",
+    "url": "/static/js/2.6885500a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00bd36ec5670e33993d1",
+    "url": "/static/js/main.d2bd5b30.chunk.js"
+  },
+  {
+    "revision": "3e234450efee118b7e12",
+    "url": "/static/js/runtime-main.fb220953.js"
+  },
+  {
+    "revision": "7b72c949c0bd79455f0cf17e09ec4269",
+    "url": "/static/media/gradient.7b72c949.png"
+  }
+]);
